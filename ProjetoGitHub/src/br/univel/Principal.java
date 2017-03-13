b4b3c7aa45a33545e8db27789ea6690f4a700ba3
Dhonatan Wesley <dwsilva.98@hotmail.com>
2017-03-13 18:52:37 -0300

    versão 5

--- ProjetoGitHub/src/br/univel/Principal.java
+++ ProjetoGitHub/src/br/univel/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		String i = "hello";
 		String msg = i + "hello !";
-		
+		System.out.println("-----------------------");
 		System.out.println(msg);
 		
 	}
